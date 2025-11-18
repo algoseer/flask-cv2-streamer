afb96f8ac629a66b2de8df5c2542894881c546ee
Naveen Kumar <naveen.kumar@disney.com>
2025-11-17 20:47:00 -0800

    added download models script

---
+++ download_models.sh
@@ -0,0 +1,3 @@
+curl -O https://raw.githubusercontent.com/pjreddie/darknet/refs/heads/master/cfg/yolov3.cfg
+curl -O https://raw.githubusercontent.com/pjreddie/darknet/refs/heads/master/data/coco.names
+curl -O https://data.pjreddie.com/files/yolov3.weights
